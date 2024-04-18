58365fb3b6068f9ffc2c20befd1303b560756545
a.pinchuk <co.born2code@gmail.com>
2024-04-18 11:49:58 +0200

    setInterval

--- bot.js
+++ bot.js
@@ -20,13 +20,13 @@ let stats = {
 // Отключаем устаревшую функцию автоматической отмены промисов
 TelegramBot.Promise = Promise;
 
-// Создаем бота с опцией {polling: true} для обработки сообщений
 const bot = new TelegramBot(token, { polling: true });
 loadHeroes();
 
+setInterval(checkForNewMatches, 240000); // Проверяем новые матчи каждые 4 минуты
+
 bot.on("message", (msg) => {
   if (!msg.text) return;
-
   const chatID = msg.chat.id;
   chatIDs.set(chatID, true);
 
@@ -42,7 +42,6 @@ async function loadHeroes() {
   try {
     const response = await fetch(DotaAPI + "/heroes");
     const heroesData = await response.json();
-
     heroes = heroesData.reduce((acc, hero) => {
       acc[hero.id] = new Hero(hero.id, hero.name, hero.localized_name);
       return acc;
@@ -52,25 +51,26 @@ async function loadHeroes() {
   }
 }
 
+async function checkForNewMatches() {
+  const lastMatchStats = await fetchLastMatchStats(PlayerID);
+  if (lastMatchStats && lastMatchStats.matchID !== lastCheckedMatchID) {
+    lastCheckedMatchID = lastMatchStats.matchID;
+    updateStats(lastMatchStats, lastMatchStats.playerName.length > 0);
+    sendMatchStats(lastMatchStats);
+  }
+}
+
 async function fetchLastMatchStats(playerID) {
   try {
     const url = `${DotaAPI}/players/${playerID}/recentMatches`;
     const response = await fetch(url);
     const recentMatches = await response.json();
 
-    if (
-      recentMatches.length > 0 &&
-      recentMatches[0].match_id !== lastCheckedMatchID
-    ) {
+    if (recentMatches.length > 0) {
       const match = recentMatches[0];
-      lastCheckedMatchID = match.match_id; // Обновляем ID последнего обработанного матча
-      const matchID = match.match_id;
-      const playerNames = await fetchMatchDetails(matchID);
-      let playerName = "";
-      if (playerNames.length > 0) {
-        playerName = playerNames.join(", ");
-      }
-      const newMatch = new Match(
+      const playerNames = await fetchMatchDetails(match.match_id);
+      const playerName = playerNames.join(", ");
+      return new Match(
         match.match_id,
         match.hero_id,
         match.kills,
@@ -80,9 +80,6 @@ async function fetchLastMatchStats(playerID) {
         match.player_slot,
         playerName
       );
-
-      updateStats(newMatch, playerNames.length > 0); // Обновляем статистику
-      return newMatch;
     }
   } catch (error) {
     console.error("Error fetching or parsing recent matches data:", error);
@@ -91,9 +88,38 @@ async function fetchLastMatchStats(playerID) {
   return new Match();
 }
 
+async function fetchMatchDetails(matchID) {
+  try {
+    const url = `https://api.opendota.com/api/matches/${matchID}`;
+    const response = await fetch(url);
+    const matchDetails = await response.json();
+
+    const validNames = new Set([
+      "Fun* [3rd]",
+      "Fun* [2nd]",
+      "Fun* [rzns]",
+      "Iлюха Звiр",
+    ]);
+    const playerNames = matchDetails.players
+      .filter((player) => validNames.has(player.personaname))
+      .map((player) => player.personaname);
+
+    return playerNames;
+  } catch (error) {
+    console.error("Error fetching match details:", error);
+    return [];
+  }
+}
+
+function sendMatchStats(lastMatchStats) {
+  const formattedMessage = formatMatchStats(lastMatchStats, stats);
+  chatIDs.forEach((chatID) => {
+    bot.sendMessage(chatID, formattedMessage);
+  });
+}
+
 function updateStats(match, playedWithFriend) {
   const didWin = match.radiantWin == match.playerSlot < 128;
-
   if (playedWithFriend) {
     stats.PlayWithFriend++;
     if (didWin) {
@@ -111,65 +137,38 @@ function updateStats(match, playedWithFriend) {
   }
 }
 
-async function fetchMatchDetails(matchID) {
-  try {
-    const url = `https://api.opendota.com/api/matches/${matchID}`;
-    const response = await fetch(url);
-    const matchDetails = await response.json();
-
-    const validNames = new Set([
-      "Fun* [3rd]",
-      "Fun* [2nd]",
-      "Fun* [rzns]",
-      "Iлюха Звiр",
-      "SpivaK",
-      "Ȼαþĭϯαņ Ǥѡƴȡĭøņ",
-      "YobyDal",
-    ]);
-    const playerNames = matchDetails.players
-      .filter((player) => validNames.has(player.personaname))
-      .map((player) => player.personaname);
-
-    return playerNames;
-  } catch (error) {
-    console.error("Error fetching match details:", error);
-    return [];
-  }
-}
-
 function formatMatchStats(match, stats) {
   const heroName = heroes[match.heroID]
     ? heroes[match.heroID].localizedName
     : "Unknown";
-  const isRadiant = match.playerSlot < 128;
-  const didWin =
-    (isRadiant && match.radiantWin) || (!isRadiant && !match.radiantWin);
   const kda =
     match.deaths === 0
       ? match.kills + match.assists
       : (match.kills + match.assists) / match.deaths;
+  const result = match.radiantWin == match.playerSlot < 128 ? "won" : "lost";
   const playWithFriendRate =
     stats.PlayWithFriend > 0
-      ? (stats.PlayWithFriendWins / stats.PlayWithFriend) * 100
-      : 0;
+      ? ((stats.PlayWithFriendWins / stats.PlayWithFriend) * 100).toFixed(2)
+      : "0.00";
   const playWithoutFriendRate =
     stats.PlayWithoutFriend > 0
-      ? (stats.PlayWithoutFriendWins / stats.PlayWithoutFriend) * 100
-      : 0;
-  const result = didWin ? "won" : "lost";
+      ? ((stats.PlayWithoutFriendWins / stats.PlayWithoutFriend) * 100).toFixed(
+          2
+        )
+      : "0.00";
 
   return (
     `Vlados last match stats:\n` +
-    `Герой - ${heroName}\n` +
+    `Hero - ${heroName}\n` +
     `K/D/A - ${match.kills}/${match.deaths}/${
       match.assists
     } (KDA: ${kda.toFixed(2)})\n` +
-    `Результат - ${result}\n` +
-    `Забущен кем - ${match.playerName}\n` +
-    `Всего игр с друзьями: ${stats.PlayWithFriend}\n` +
-    `Всего игр без друзей: ${stats.PlayWithoutFriend}\n` +
-    `Win rate играя с друзьями: ${playWithFriendRate.toFixed(2)}%\n` +
-    `Win rate играя без друзей: ${playWithoutFriendRate.toFixed(2)}%`
+    `Result - ${result}\n` +
+    `Played with - ${match.playerName}\n` +
+    `Total games with friends: ${stats.PlayWithFriend}\n` +
+    `Total games without friends: ${stats.PlayWithoutFriend}\n` +
+    `Win rate with friends: ${playWithFriendRate}%\n` +
+    `Win rate without friends: ${playWithoutFriendRate}%`
   );
 }
 
@@ -194,9 +193,9 @@ class Match {
   ) {
     this.matchID = matchID;
     this.heroID = heroID;
-    this.kills = kills || 0; // Добавьте значения по умолчанию, чтобы избежать undefined
-    this.deaths = deaths || 0;
-    this.assists = assists || 0;
+    this.kills = kills;
+    this.deaths = deaths;
+    this.assists = assists;
     this.radiantWin = radiantWin;
     this.playerSlot = playerSlot;
     this.playerName = playerName;
